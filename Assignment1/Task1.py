221d3e9df7abfc7bbb0a3bfcd1511b9c98a84c3a
madhur0014 <97623052+madhur0014@users.noreply.github.com>
2025-06-09 00:31:32 +0530

    Task1.py

---
+++ Assignment1/Task1.py
@@ -0,0 +1,7 @@
+a = float(input("Input first number: "))
+b = float(input("Input second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
