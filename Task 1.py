88a1c7d436934600843f451842f4e18442df72d8
madhur0014 <97623052+madhur0014@users.noreply.github.com>
2025-06-09 00:15:52 +0530

    Add files via upload
    Added Task 1 and Task 2 Python scripts

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  8 23:34:42 2025
+
+@author: 61198
+"""
+
+a = float(input("Input first number: "))
+b = float(input("Input second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
